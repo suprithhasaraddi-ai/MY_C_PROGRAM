41cea43b4641e378be0351252d1052a1ed28da5d
suprithhasaraddi-ai <suprithhasaraddi@gmail.com>
2025-11-18 12:32:07 +0530

    Update break staetment.c

--- break staetment.c
+++ break staetment.c
@@ -5,10 +5,10 @@ int main() {
 
     for(i = 1; i <= 10; i++) {
         if(i == 5) {
-            break;   // Loop stops when i becomes 5
+            break;   
         }
         printf("%d\n", i);
     }
 
-    return 0;
+    
 }
