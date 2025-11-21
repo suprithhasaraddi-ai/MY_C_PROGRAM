e188f2ea6038b0a64849adc8ff2fb5873704bb98
suprithhasaraddi-ai <suprithhasaraddi@gmail.com>
2025-11-21 15:34:23 +0530

    Create check the student has taken the passing marks lab.c

---
+++ check the student has taken the passing marks lab.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int checkPass(int m1, int m2, int m3) {
+    if (m1 >= 40 && m2 >= 40 && m3 >= 40)
+        return 1; 
+        else 
+        return 0;
+}
+int main() {
+    int s1, s2, s3;
+    float avg;
+    printf("Enter marks of three subjects: ");
+    scanf("%d %d %d", &s1, &s2, &s3);
+    avg = (s1 + s2 + s3) / 3.0;
+    printf("Average = %.2f\n", avg);
+    if (checkPass(s1, s2, s3))
+        printf("Result: PASS\n");
+    else
+        printf("Result: FAIL\n");
+
+    ret
